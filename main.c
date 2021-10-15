4a4876cba7632c8b1dea9e844215f1ccb5ba1b7b
brevian <92574258+brevian@users.noreply.github.com>
2021-10-15 11:52:06 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+/*-------------------
+Project:Library management system
+Author:Brevian 
+Compiler:gcc
+Licence:MIT
+Date:14/10/2021
+---------------------*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    printf("\t County library management system\n");
+    printf("1:Login\n");
+    printf("Welcome Brevian\n");
+    printf("What do you want to do?\n");
+    printf("2:Add a user\n");
+    printf("3:Add a book\n");
+    printf("4:Borrow a book\n");
+    printf("5:Return book\n");
+    printf("6:Replace a book\n");
+    printf("7:Update User\n");
+    printf("8:Delete user\n");
+    printf("9:Logout\n");
+    printf("Good bye!!\n");
+    return 0;
+}
